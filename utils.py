94eb5326171390f0d93ef73e0357db26598fad73
Ane1y <9112539@mail.ru>
2022-04-10 01:11:13 +0300

    add rename function

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 import os
+
 path = './test/data/TEMP'
-files = os.listdir(path)
-i = 0
-for file in files:
-   os.rename(os.path.join(path, file), os.path.join(path, 'xiao_' + str(i) + '.jpg'))
-   i = i + 1
+
+def rename_files_in_dir(path):
+   files = os.listdir(path)
+   i = 0
+   for file in files:
+      os.rename(os.path.join(path, file), os.path.join(path, 'xiao_' + str(i) + '.jpg'))
+      i = i + 1
